87c7cb69ff63350921e6de7e883c88bdc8ab9e02
Ethan Watts <ethan.watts@live.co.uk>
2024-02-14 11:34:58 +0000

    w2 e1 done

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -6,16 +6,33 @@
 
 // to blink or not to blink...
 bool doBlinking = true;
+char MAC_ADDRESS[13];
+
 
 void setup() {
   Serial.begin(115200);
   Serial.println("arduino started");
   pinMode(LED_BUILTIN, OUTPUT);
+
+  getMAC(MAC_ADDRESS);
 } // setup
+
+void getMAC(char *buf) { // the MAC is 6 bytes, so needs careful conversion...
+  uint64_t mac = ESP.getEfuseMac(); // ...to string (high 2, low 4):
+  char rev[13];
+  sprintf(rev, "%04X%08X", (uint16_t) (mac >> 32), (uint32_t) mac);
+
+  // the byte order in the ESP has to be reversed relative to normal Arduino
+  for(int i=0, j=11; i<=10; i+=2, j-=2) {
+    buf[i] = rev[j - 1];
+    buf[i + 1] = rev[j];
+  }
+  buf[12] = '\0';
+}
 
 void loop() {
   Serial.printf("\nahem, hello (merged bin) world\n");
-  Serial.printf(getEfuseMac());
+  Serial.printf("MAC Address: %s\n", MAC_ADDRESS);
 
   #ifdef ESP_IDF_VERSION_MAJOR
     Serial.printf( // IDF version
