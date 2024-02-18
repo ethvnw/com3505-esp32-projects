f04d6bdd037ca9c51bb46a0739c79cbd444f403d
Ethan Watts <ethan.watts@live.co.uk>
2024-02-18 18:12:50 +0000

    add get mac address code back

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,35 @@
 #include <Arduino.h>
 
-// put function declarations here:
 #define LED_PIN 6
 #define SWITCH_PIN 5
+char MAC_ADDRESS[13];
+
+
+void getMAC(char *buf) { // the MAC is 6 bytes, so needs careful conversion...
+  uint64_t mac = ESP.getEfuseMac(); // ...to string (high 2, low 4):
+  char rev[13];
+  sprintf(rev, "%04X%08X", (uint16_t) (mac >> 32), (uint32_t) mac);
+
+  // the byte order in the ESP has to be reversed relative to normal Arduino
+  for(int i=0, j=11; i<=10; i+=2, j-=2) {
+    buf[i] = rev[j - 1];
+    buf[i + 1] = rev[j];
+  }
+  buf[12] = '\0';
+}
+
 
 void setup() {
   Serial.begin(115200);
   pinMode(LED_PIN, OUTPUT);
   pinMode(SWITCH_PIN, INPUT_PULLUP);
 
-  Serial.println("do i work");
-
+  getMAC(MAC_ADDRESS);
 }
 
+
 void loop() {
-  Serial.println("Testing exercise 2");
+  Serial.printf("MAC Address: %s\n", MAC_ADDRESS);
 
   if (digitalRead(SWITCH_PIN) == LOW) {
     Serial.println("Button pressed!");
