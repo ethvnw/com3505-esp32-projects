016a8eb7daa6ca7a2142f070f2ba1e648c6042ca
Ethan Watts <ethan.watts@live.co.uk>
2024-02-20 16:06:37 +0000

    wk3 ex 1 done

--- src/main.cpp
+++ src/main.cpp
@@ -34,27 +34,22 @@ void setup() {
 
 
 void loop() {
-  Serial.printf("MAC Address: %s\n", MAC_ADDRESS);
+  // Serial.printf("MAC Address: %s\n", MAC_ADDRESS);
+  // delay(1000);
 
   if (digitalRead(SWITCH_PIN) == LOW) {
     Serial.println("Switch is pressed");
     digitalWrite(RED, HIGH);
-    digitalWrite(YELLOW, LOW);
-    digitalWrite(GREEN, LOW);
     delay(1000);
 
-    digitalWrite(RED, LOW);
     digitalWrite(YELLOW, HIGH);
-    digitalWrite(GREEN, LOW);
     delay(1000);
 
     digitalWrite(RED, LOW);
     digitalWrite(YELLOW, LOW);
     digitalWrite(GREEN, HIGH);
     delay(1000);
-
-    digitalWrite(RED, LOW);
-    digitalWrite(YELLOW, LOW);
+    
     digitalWrite(GREEN, LOW);
   } 
 
